3943fe1b8b2e7e8f7b417ef43dd67623125ce599
Mustafa EREN <33000530+coderentr@users.noreply.github.com>
2020-07-18 11:03:41 +0300

    .

---
+++ MarketApi/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [Route("GetProductList")]
+        public IActionResult GetProductList()
+        {
+            return Ok(new List<string> { "Leptop", "Phone", "Pen" });
+        }
+    }
+}
---
+++ SalesApi/Controllers/SalesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace SalesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalesController : ControllerBase
+    {
+            [Route("sales")]
+            public IActionResult Sales()
+            {
+                return Ok("Satın alma işleminiz gerçekleştirildi.");
+            }
+    }
+}
